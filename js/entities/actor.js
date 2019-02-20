23b0c1ae9ef625cc2ad4f03542eafb129d26345f
Victor DESFOURNEAUX <victor.desfourneaux@etudiant.univ-reims.fr>
2019-02-20 21:41:25 +0100

    stats

--- js/entities/actor.js
+++ js/entities/actor.js
@@ -2,6 +2,15 @@ class Actor extends Entity {
   constructor(game, config) {
     super(game, config);
     this.ia = config.ia; // undefined possible
+    this.stats = {
+      hpMax: config.stats.hpMax, // points de vie max
+      mpMax: config.stats.mpMax, // points de magie
+      strength: config.stats.strength, // force
+      defense: config.stats.defense, // défense
+      dodgeRate: config.stats.dodgeRate // taux d'esquive
+    };
+    this.stats.hp = this.stats.hpMax;
+    this.stats.mp = this.stats.mpMax;
   }
   update() {
     if (this.ia !== undefined)
--- js/entities/adventurers/swordsman.js
+++ js/entities/adventurers/swordsman.js
@@ -3,10 +3,19 @@ class Swordsman extends Adventurer {
     super(game, {
       x: x,
       y: y,
-      sprite: 'swordsman'
+      sprite: 'swordsman',
+      stats: {
+        hpMax: 1,
+        mpMax: 1,
+        strength: 1,
+        defense: 1,
+        dodgeRate: 1
+      }
     });
   }
   static preload(game) {
     game.load.image('swordsman', 'sprites/swordsman_idle0.png');
   }
 }
+
+Swordsman.price = 1;
