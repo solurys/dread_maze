4c07e6b2f59899509759b519071803ce3e6a4ff8
Victor DESFOURNEAUX <victor.desfourneaux@etudiant.univ-reims.fr>
2019-02-22 15:51:13 +0100

    FilterTargets : ajout de documentation

--- js/utils/filter_targets_helpers.js
+++ js/utils/filter_targets_helpers.js
@@ -5,32 +5,42 @@
 
 /************* les filtres *************/
 
+// ne garde que les cibles en vie.
 FilterTargets.prototype.alive = function() {
   return this.filter(e => e.alive);
 }
 
+// ne garde que les cibles mortes
 FilterTargets.prototype.dead = function() {
   return this.filter(e => !e.alive);
 }
 
+// ne garde que le type de cible donné par une classe
+// par exemple : .ofType(Adventurer)
 FilterTargets.prototype.ofType = function(type) {
   return this.filter(e => e instanceof type);
 }
 
+// ne garde que les ennemis proches selon un cercle de rayon "range" pixels
 FilterTargets.prototype.nearby = function(range) {
   var s = this.self;
   return this.filter(e => Math2D.pixelDistance2(s, e) <= range**2);
 }
 
+// ne garde que les ennemis plus loin qu'un cercle de rayon "range" pixels
 FilterTargets.prototype.faraway = function(range) {
   var s = this.self;
   return this.filter(e => Math2D.pixelDistance2(s, e) >= range**2);
 }
 
+// permet de filtrer selon les stats de la cible (avec une fonction donnée)
+// par exemple : .filterStats(target => target.hp < 10)
 FilterTargets.prototype.filterStats = function(f) {
   return this.filter(e => f(e.stats));
 }
 
+// permet de comparer les stats de la cible et de soi-même (avec une fonction donnée)
+// par exemple : .compareStats( (self,target) => self.hp > target.hp )
 FilterTargets.prototype.compareStats = function(f) {
   var self = this.self;
   return this.filter(target => f(self.stats, target.stats));
@@ -39,7 +49,8 @@ FilterTargets.prototype.compareStats = function(f) {
 
 /************* les tris *************/
 
-
+// tri par distance en pixels
+// soit ascendant (par défaut, proche en premier) ou descendant (loin en premier)
 FilterTargets.prototype.sortByDistance = function(ascending = true) {
   var s = this.self;
   if (ascending) { // plus proche en premier
@@ -57,6 +68,9 @@ FilterTargets.prototype.sortByDistance = function(ascending = true) {
   return this;
 }
 
+// tri par une stat
+// soit ascendant (par défaut, petit en premier) ou descendant (grand en premier)
+// par exemple : .sortByStat('hp')
 FilterTargets.prototype.sortByStat = function(stat, ascending = true) {
   if (ascending)
     this.sort((e1, e2) => e1.stats[stat] - e2.stats[stat]);
---
+++ js/utils/filter_targets_tuto.md
@@ -0,0 +1,47 @@
+Pour construire une requête avec filterTargets, il faut généralement spécifier dans l'ordre :
+- les filtres (réduit le nombre de possibilités)
+- les tris (donne un ordre)
+- l'accesseur (juste le premier ou tout le tableau)
+
+Par exemple, pour avoir l'aventurier le plus proche, distant de moins de 200px,
+relativement à *myEntity* :
+```js
+var target =
+ filterTargets(myEntity, [entityManager.adventurers])
+  // filtres
+  .alive() // en vie, pas mort
+  .nearby(200) // distant de 200px
+  // tris
+  .sortByDistance() // le plus proche devient le premier
+  // accesseur
+  .first()
+```
+
+Si aucune cible ne correspond, target aura la valeur *undefined*
+(ou dans le cas de .toArray(), un tableau vide).  
+Il faut donc tester la valeur de target avant de l'utiliser :
+```js
+if (target !== undefined) {
+// Attaquer la cible ? Soigner un allié ?
+}
+```
+
+On peut aussi mélanger les résultats pour avoir un aventurier aléatoire :
+```js
+var aventurierAleatoire =
+ filterTargets(myEntity, [entityManager.adventurers])
+  .alive()
+  .shuffle()
+  .first()
+```
+
+Si on spécifie plusieurs tris, il seront effectués par priorité.  
+On peut par exemple trier par hp, et s'il ont les même hp par distance :
+```js
+var target_array =
+ filterTargets(myEntity, [entityManager.adventurers])
+  .alive()
+  .sortByStat('hp')
+  .sortByDistance()
+  .toArray()
+```
