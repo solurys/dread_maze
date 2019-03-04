4c3288fc94261bbf6ac096856fc945edcf396154
gabriel <gabriel@localhost.localdomain>
2019-03-04 20:01:22 +0100

    monstre Cac avanacement sans A*

---
+++ js/ia/ia_monstre_cac.js
@@ -0,0 +1,61 @@
+// classe IA de base
+
+class MonstreCac extends IA {
+  // self : entité controllée par l'ia
+  constructor(self) {
+    super(self);
+    this.etat = 1; // état inerte par défaut
+    this.target = null;
+    this.destination = {x: this.self.x, y: this.self.x};
+    this.speed = 100;
+  }
+  update() {
+    var that = this;
+      function detecterProche () {
+          return filterTargets(that.self, [that.self.game.entityManager.adventurers])
+                  // filtres
+                  .alive() // en vie, pas mort
+                  .nearby(200) // distant de moins de 200px
+                  // tris
+                  .sortByDistance() // le plus proche devient le premier
+                  // accesseur
+                  .first();
+      }
+      function mouvementAleatoire() {
+        var epsilon = 10;
+        if (Math.abs(that.destination.x - that.self.x) < epsilon && Math.abs(that.destination.y - that.self.y) < epsilon){
+          that.destination = {x: Phaser.Math.between(0,800), y: Phaser.Math.between(0,600)}; //limitation à la salle
+        }
+        else {
+          var vel = Vector.from_to(that.self, that.destination).normalize().multiply(that.speed);
+          that.self.body.velocity.setTo(vel.x, vel.y);
+        }
+      }
+
+      switch(this.etat) {
+          case 1: //patrouille
+            //le personnage se déplace aléatoirement dans la piece
+            mouvementAleatoire();
+            //si ennemie detecter etat attaque
+            this.target = detecterProche();
+            if (this.target != undefined)
+                this.etat = 2;
+            break;
+        case 2: //attaque
+            if (Math2D.pixelDistance2(this.self, this.target) < 200**2) {
+              this.self.attack(this.target);
+            }
+
+            if (Math2D.pixelDistance2(this.self, this.target) > 200**2) {
+              this.etat = 1;
+            }
+
+            else{
+              var vel = Vector.from_to(that.self, that.target).normalize().multiply(that.speed);
+              this.self.body.velocity.setTo(vel.x, vel.y);
+            }
+      }
+  }
+  debug() {}
+
+}
--- js/states/preparation.js
+++ js/states/preparation.js
@@ -2,17 +2,17 @@ class PreparationState extends Phaser.State {
   create(game) {
     // temporaire
 
-    var sw1 = game.entityManager.add(new Swordsman(game, 0, 0));
-    sw1.ia = new KeyboardArrowControl(sw1);
+    // var sw1 = game.entityManager.add(new Swordsman(game, 0, 0));
+    // sw1.ia = new KeyboardArrowControl(sw1);
 
     //var sw2 = game.entityManager.add(new Swordsman(game, 100, 100));
     //sw2.ia = new KeyboardArrowControl(sw2);
 
-    var sw3 = game.entityManager.add(new Swordsman(game, 200, 200));
+    var sw3 = game.entityManager.add(new Swordsman(game, 100, 100));
     sw3.ia = new KeyboardZQSDControl(sw3);
 
-    var sw4 = game.entityManager.add(new Swordsman(game, 300, 300));
-    sw4.ia = new FollowEnemy(sw4, [game.entityManager.adventurers]);
+     var sw4 = game.entityManager.add(new Swordsman(game, 300, 300));
+      sw4.ia = new  MonstreCac(sw4);//FollowEnemy(sw4, [game.entityManager.adventurers]);
 
     game.state.start('fight', false);
   }
