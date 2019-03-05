07eaba1d5d1b0826eff4f5083ecbb155a5ef0556
Jack CARBONERO <jack.carbonero@etudiant.univ-reims.fr>
2019-03-05 21:19:41 +0100

    Début accueil

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 game = new Phaser.Game(800, 600, Phaser.CANVAS);
 
 game.state.add('init', InitState);
+game.state.add('menu', MenuState);
 game.state.add('preparation', PreparationState);
 game.state.add('fight', FightState);
 
--- js/states/init.js
+++ js/states/init.js
@@ -4,16 +4,15 @@ class InitState extends Phaser.State {
     Entity.preload(game);
     HUD.preload(game);
     // l'entityManager gère la logique de jeu
-    game.entityManager = new EntityManager(game);
+    
     
     game.stage.backgroundColor = "#4488AA";
 
   }
   create(game) {
     // passage à la préparation du dongeon
-    game.state.start('preparation', false);
+    game.state.start('menu', false);
 
-    // Création de l'HUD
-    var h = new HUD(game);
+    
   }
 }
