8067b2babe77269b979647227f97fbbe0dafb2f3
Victor DESFOURNEAUX <victor.desfourneaux@etudiant.univ-reims.fr>
2019-03-07 00:31:49 +0100

    lpc atlas et skeleton

---
+++ js/utils/lpc-atlas.js
@@ -0,0 +1,20 @@
+function setup_lpc_animations(sprite) {
+  const fps = 10;
+  function addAnim(name, length) {
+    var frames = Phaser.ArrayUtils.numberArray(length).map(num => name + '-' + num);
+    sprite.animations.add(name, frames, fps);
+  }
+  function addAnim4Dir(name, length) {
+    addAnim(name + '-left', length);
+    addAnim(name + '-right', length);
+    addAnim(name + '-up', length);
+    addAnim(name + '-down', length);
+  }
+
+  addAnim4Dir('spellcast', 7);
+  addAnim4Dir('thrust', 8);
+  addAnim4Dir('walk', 9);
+  addAnim4Dir('slash', 6);
+  addAnim4Dir('shoot', 13);
+  addAnim('die', 6);
+}
---
+++ js/utils/lpc-atlas.py
@@ -0,0 +1,51 @@
+def initAtlas(img, w, h, format):
+    return {
+        "frames": {},
+        "meta": {
+            "version": "1.0",
+            "image": img,
+            "format": format,
+            "size": {"w":w, "h":h},
+            "scale": "1"
+        }
+    }
+
+def addFrame(atlas, name, x, y, w=64, h=64):
+    atlas['frames'][name] = {
+        "frame": {"x":x, "y":y, "w":w, "h":h},
+        "rotated": False,
+        "trimmed": False,
+        "spriteSourceSize": {"x":0, "y":0, "w":w, "h":h},
+        "sourceSize": {"w":w, "h":h}
+    }
+
+def animLine(atlas, n, basename, y, w=64, h=64, suffix='-'):
+    for i in range(0, n):
+        x = i*w
+        name = basename + suffix + str(i)
+        addFrame(atlas, name, x, y, w, h)
+
+def anim4DirLines(atlas, n, basename, y, w=64, h=64):
+    animLine(atlas, n, basename+'-up',    y,     w, h)
+    animLine(atlas, n, basename+'-left',  y+h,   w, h)
+    animLine(atlas, n, basename+'-down',  y+2*h, w, h)
+    animLine(atlas, n, basename+'-right', y+3*h, w, h)
+
+
+def line(l, h=64):
+    return l*h
+
+atlas = initAtlas('skeleton.png', 832, 1344, 'RGBA8888')
+
+anim4DirLines(atlas, 7, 'spellcast', line(0))
+anim4DirLines(atlas, 8, 'thrust', line(4))
+anim4DirLines(atlas, 9, 'walk', line(8))
+anim4DirLines(atlas, 6, 'slash', line(12))
+anim4DirLines(atlas, 13, 'shoot', line(16))
+animLine(atlas, 6, 'die', line(20))
+
+import json
+
+print(json.dumps(atlas, sort_keys=True, indent=4))
+
+# python3 lpc-atlas.py > skeleton_atlas.json
