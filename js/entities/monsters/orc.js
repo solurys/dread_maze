c6787a82a0d21345502b122ebdd988cb805c548f
Victor Desfourneaux <victor.desfourneaux@etudiant.univ-reims.fr>
2019-03-07 13:30:22 +0100

    resolution du bug animation

--- js/entities/monsters/orc.js
+++ js/entities/monsters/orc.js
@@ -20,7 +20,7 @@ class Orc extends Monster {
     // this.animations.add('walk-right',[27,28,29,30,31,32,33,34,35],10);
     setup_lpc_animations(this);
 
-    this.ia = new MonstreCac(this, 70);
+    this.ia = new MonstreCac(this, 70,300, 400);
   }
   static preload(game) {
     game.load.atlasJSONHash('orc', 'sprites/Monsters/Orc/orc.png', 'sprites/Monsters/Skeleton/skeleton_atlas.json');
