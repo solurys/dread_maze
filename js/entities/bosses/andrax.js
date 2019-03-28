d4b3a9a2caee3ab62d896959310f3fd74e145d45
Victor Desfourneaux <victor.desfourneaux@etudiant.univ-reims.fr>
2019-03-28 13:24:53 +0100

    body oversize

--- js/entities/bosses/andrax.js
+++ js/entities/bosses/andrax.js
@@ -11,7 +11,8 @@ class Andrax extends Boss {
         defense: 2,
         dodgeRate: 0.3
       },
-      baseAttack: 'dragonSpear'
+      baseAttack: 'dragonSpear',
+      oversize: true
     });
     // this.animations.add('walk-up',[0,1,2,3,4,5,6,7,8],10);
     // this.animations.add('walk-left',[9,10,11,12,13,14,15,16,17],10);
