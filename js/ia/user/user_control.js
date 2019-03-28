50253ac3de81147ae72dba0f5ffc1c11065c89bb
Victor Desfourneaux <victor.desfourneaux@etudiant.univ-reims.fr>
2019-03-28 13:07:12 +0100

    attaque joueur

--- js/ia/user/user_control.js
+++ js/ia/user/user_control.js
@@ -20,9 +20,13 @@ class UserControl extends IA {
     if (k.left.isDown)  vel.x -= this.speed;
     if (k.right.isDown) vel.x += this.speed;
 
+    var e = filterTargets(this.self, [this.self.game.entityManager.adventurers])
+      .nearby(60)
+      .first();
+
 
     if (k.attack.justPressed())
-      this.self.attack();
+      this.self.attack(e);
     else if (vel.x != 0 || vel.y != 0)
       this.self.walk(vel);
     else
