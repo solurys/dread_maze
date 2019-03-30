de8cae265b859abda4ec52f2e9fb9f8305b7bccd
Victor DESFOURNEAUX <victor.desfourneaux@etudiant.univ-reims.fr>
2019-03-30 15:08:01 +0100

    diminution de la taille des entités

--- js/entities/entity.js
+++ js/entities/entity.js
@@ -1,6 +1,7 @@
 class Entity extends Phaser.Sprite {
   constructor(game, config) {
     super(game, config.x, config.y, config.sprite, config.frame);
+    this.scale.setTo(0.6,0.6);
     this.debugEnabled = true; // gameDebug
   }
   static preload(game) {
